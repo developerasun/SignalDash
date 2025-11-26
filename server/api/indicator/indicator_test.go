96e53baa5453d721c3e4bd3cc3825d170c5f73a8
developerasun <nellow1102@gmail.com>
2025-11-27 00:00:44 +0900

    test: add test case for cron lib

--- server/api/indicator/indicator_test.go
+++ server/api/indicator/indicator_test.go
@@ -2,14 +2,18 @@ package indicator
 
 import (
 	"testing"
+	"time"
 
+	"github.com/developerasun/SignalDash/server/config"
 	"github.com/developerasun/SignalDash/server/models"
+	"github.com/robfig/cron/v3"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func TestIndicatorInsert(t *testing.T) {
+	t.Skip()
 	assert := assert.New(t)
 	testdb, oErr := gorm.Open(sqlite.Open("../../sd_app.test.db"), &gorm.Config{})
 
@@ -27,4 +31,31 @@ func TestIndicatorInsert(t *testing.T) {
 	})
 
 	assert.Nil(cErr)
+}
+
+func TestCronExpression(t *testing.T) {
+	assert := assert.New(t)
+	c := cron.New()
+
+	options := &config.ViperOptions{
+		Filename:  "options",
+		ConfigDir: "../../config",
+	}
+	v := options.InitConfig()
+
+	expression := v.GetString("cron.expression.every1min")
+
+	count := 0
+	c.AddFunc(expression, func() {
+		if count < 3 {
+			t.Log(time.Now().UTC())
+			count++
+		} else {
+			c.Stop()
+		}
+	})
+
+	<-time.After(time.Second * 3)
+
+	assert.Equal(count, 2)
 }
